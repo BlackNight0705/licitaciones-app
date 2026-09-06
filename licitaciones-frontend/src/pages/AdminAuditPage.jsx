2f2b638986dc22ca042a259a3fa5cf27962c23af
BlackNight0507 <abdelgamer0001@gmail.com>
2026-09-05 21:45:30 -0500

    Ajustes En Controles5

--- licitaciones-frontend/src/pages/AdminAuditPage.jsx
+++ licitaciones-frontend/src/pages/AdminAuditPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import api from '../../api/axiosClient';
+import api from '../api/axiosClient';
 
 export default function AdminAuditPage() {
   const [logs, setLogs] = useState([]);
